0ce3bee9720a745d7226c48adb128a417aae292d
Kasper Karlgren <kaka3286@C523.local>
2023-10-27 16:12:12 +0900

    added wheel buttons

--- app.py
+++ app.py
@@ -30,6 +30,18 @@ def color():
     sendToArduino(data['c'])
     return make_response(jsonify("success"), 201)
 
+@app.route("/wheels", methods=["POST"])
+def wheels():
+
+    data = json.loads(request.form['json'])
+    print(data)
+    print(data['w'])
+    print('wheels moving!', file=sys.stderr)
+    sendToArduino(data['w'])
+    return make_response(jsonify("success"), 201)
+
+
+
 
 def sendToArduino(message):
     ser = serial.Serial('/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_0178C8A8-if00-port0', 115200, timeout=1)
--- client/src/App.svelte
+++ client/src/App.svelte
@@ -11,6 +11,21 @@
 	 name: 'Blue'
  }];
 
+ let wheellist= [{
+	 id: "w",
+	 name: 'Forward'
+ }, {
+	 id: "a",
+	 name: 'Left'
+ }, {
+	 id: "d",
+	 name: 'Right'
+ }, {
+     id: "s",
+	 name: 'Back'
+ }];
+
+
  let rand = -1;
  function getRand() {
      fetch("./rand")
@@ -18,16 +33,31 @@
          .then(d => (rand = d));
  }
  async function sendCol(color) {
-    console.log(color)
-    var obj = {'c': color}
+     console.log(color)
+     var obj = {'c': color}
 
-    var dataToSend = new FormData();
+         var dataToSend = new FormData();
      dataToSend.append( "json", JSON.stringify( obj ) );
 
      const res = await fetch('./color', {
          method: "POST",
          body: dataToSend
-        })
+     })
+     const json = await res.json()
+	 console.log(JSON.stringify(json))
+ }
+
+ async function sendWheel(direction) {
+     console.log(direction)
+     var obj = {'w': direction}
+
+         var dataToSend = new FormData();
+     dataToSend.append( "json", JSON.stringify( obj ) );
+
+     const res = await fetch('./color', {
+         method: "POST",
+         body: dataToSend
+     })
      const json = await res.json()
 	 console.log(JSON.stringify(json))
  }
@@ -41,25 +71,28 @@
     <button on:click={() => sendCol(col.id)}>{col.name}</button>
 {/each}
 
+{#each wheellist as dir}
+    <button on:click={() => sendWheel(dir.id)}>{dir.name}</button>
+{/each}
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+ main {
+	 text-align: center;
+	 padding: 1em;
+	 max-width: 240px;
+	 margin: 0 auto;
+ }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+ h1 {
+	 color: #ff3e00;
+	 text-transform: uppercase;
+	 font-size: 4em;
+	 font-weight: 100;
+ }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+ @media (min-width: 640px) {
+	 main {
+		 max-width: none;
+	 }
+ }
 </style>
