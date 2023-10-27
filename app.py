9626ef6130c4c867b2dbc3c59aa858570551eab7
Kasper Karlgren <kaka3286@C523.local>
2023-10-27 14:42:46 +0900

    Encode change

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def sendToArduino(message):
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser.reset_input_buffer()
     while True:
-        ser.write(message.encode())
+        ser.write(message.encode('utf-8'))
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
 
