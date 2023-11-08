1dcb56a1b73c5f9ef4a7cc6e0eae23a8dfe36794
Kasper Karlgren <kaka3286@C523.local>
2023-11-08 15:03:28 +0900

    changes a double line typo

--- app.py
+++ app.py
@@ -38,9 +38,6 @@ from threading import Condition
 
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutputimport picamera2 #camera module for RPi camera
-from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
 
