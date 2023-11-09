25d2af92e26bf74bd938be58746b9e984bbef5e3
Kasper Karlgren <kaka3286@C523.local>
2023-11-09 13:15:28 +0900

    indent error

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def genFrames():
 #         frame = camera.get_frame()
 #         yield (b'--frame\r\n'
 #                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'
-               )
+#               )
 
 @app.route('/video_feed')
 def video_feed():
