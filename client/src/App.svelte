2c4a5b4b541e91e159b67710f16ea82dea35866a
Kasper Karlgren <kaka3286@C523.local>
2023-11-20 17:30:20 +0900

    flipepd image

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -170,5 +170,14 @@
  }
  .column {
      float: left;
+ }
+img {
+     -webkit-transform: scaleX(-1);
+     -moz-transform: scaleX(-1);
+     -o-transform: scaleX(-1);
+     transform: scaleX(-1);
+
+     filter: FlipH;
+     -ms-filter: "FlipH";
 }
 </style>
